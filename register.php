af5c7ac79571fdd363ec56efe48e26761c0805f6
jasminepopa3 <47035977+jasminepopa3@users.noreply.github.com>
2023-11-23 20:46:55 +0200

    Update register.php

--- register.php
+++ register.php
@@ -8,7 +8,7 @@
         body {
             font-family: Arial, sans-serif;
             text-align: center;
-            margin: 50px;
+            margin: 0;
         }
 
         h2 {
@@ -44,17 +44,120 @@
         }
 
         a {
-            color: #4caf50;
+            color: #1C8145;
             text-decoration: none;
         }
 
         a:hover {
             text-decoration: underline;
         }
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande',
+    'Lucida Sans', Arial, sans-serif;
+}
+
+nav {
+  height: 80px;
+  background: #3399ff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0rem calc((100vw - 1300px) / 2);
+}
+
+nav a {
+  text-decoration: none;
+  color: #000;
+  padding: 0 1.5rem;
+}
+
+nav a:hover {
+  color: #fff;
+}
+
+/*dropdown*/
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a, .dropbtn {
+  display: inline-block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover, .dropdown:hover .dropbtn {
+  background-color: red;
+}
+
+li.dropdown {
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+.dropdown-content a:hover {background-color: #f1f1f1;}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+
+
+/*stop*/
+
+.logo {
+  color: #000;
+  font-size: 1rem;
+  font-weight: bold;
+  font-style: italic;
+  padding: 0 2rem;
+}
+.hero {
+    background: #80bfff;
+    width: 100%;
+    height: 100vh; 
+    padding: 50px; 
+    box-sizing: border-box; 
+}
+
     </style>
 </head>
 <body>
-
+<nav>
+    <div class="logo">Spitalul de Urgență FMI</div>
+</nav>
+<div class="hero">
 <h2>Creare Cont</h2>
 
 <form action="process_register.php" method="post">
@@ -84,6 +187,6 @@
 </form>
 
 <p>Ai deja un cont? <a href="index.php">Autentificare</a>.</p>
-
+</div>
 </body>
 </html>
