5039731ee606e17d8a8c24e682abf21f34e5aa98
jasminepopa3 <47035977+jasminepopa3@users.noreply.github.com>
2023-11-23 20:12:59 +0200

    Add files via upload

---
+++ home_page.php
@@ -0,0 +1,73 @@
+
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="author" content="Jasmine"/>
+    <meta name="description" content="Activitatile spitalului FMI"/>
+    <meta name="keywords" content="Spital">
+
+
+
+    <title>Activitatile spitalului FMI</title>
+
+    <link rel="stylesheet" href="home_page.css" />
+  </head>
+
+
+  <body>
+    <nav>
+        <div class="logo">Spitalul de Urgență FMI</div>
+        <div class="nav-items">
+          <ul>
+            <li><a href="home_page.html">Home</a></li>
+            <li class="dropdown">
+                <a href="">Secții</a> 
+                <div class="dropdown-content">
+                    <a href="">Cardiologie</a>
+                    <a href="">Ortopedie</a>
+                    <a href="">UPU</a>
+                    <a href="">Pediatrie</a>
+                    <a href="">ATI</a>
+                    <a href="">Gastroenterologie</a>
+                    <a href="">Pneumologie</a>
+                </div>
+            </li>
+            <li>
+                <a href="">Fișe pacienți</a>
+            </li>
+            <li class="dropdown">
+                <a href="">Contul meu</a>
+                <div class="dropdown-content">
+                    <a href="">Schimbă parola</a>
+                    <a href="">Logout</a>
+                </div>
+            </li>
+          </ul>
+        </div>
+    </nav>
+
+    <section class="hero">
+      <div class="hero-container">
+        <div class="column-left">
+          <h1>Viața e o călătorie. Fiecare are dreptul de a se bucura de ea.</h1>
+          <p>
+            Suntem o echipă de profesioniști. Hai sa ajutăm oamenii. Programările și urgențele te așteaptă.
+          </p>
+          <a href=""><button>Secții</button></a>
+        </div>
+        <div class="column-right">
+          <img
+            id="hero-image"
+            src="fmi.png"
+            alt="illustration"
+            class="hero-image"
+          />
+        </div>
+      </div>
+    </section>
+
+
+  </body>
+</html>
+
