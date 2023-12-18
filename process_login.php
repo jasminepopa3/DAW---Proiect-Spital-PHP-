796c9341bafa369bfe9192dc65339b0d9c76fb48
jasminepopa3 <47035977+jasminepopa3@users.noreply.github.com>
2023-12-18 23:02:00 +0200

    Update process_login.php

--- process_login.php
+++ process_login.php
@@ -1,5 +1,25 @@
-<?php
 
+<?php
+session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
+// Verificare reCAPTCHA
+$secretKey = "6LeNqDUpAAAAAHXdZ-oencWdG8i1qJbOndZBEQOf";
+$response = $_POST['g-recaptcha-response'];
+$remoteIP = $_SERVER['REMOTE_ADDR'];
+
+$url = "https://www.google.com/recaptcha/api/siteverify?secret=$secretKey&response=$response&remoteip=$remoteIP";
+$recaptchaData = json_decode(file_get_contents($url));
+
+if (!$recaptchaData->success) {
+    // Răspunsul reCAPTCHA nu este valid
+    echo "Eroare reCAPTCHA!";
+} else {
+    // Răspunsul reCAPTCHA este valid
+
+ 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Conectare la baza de date 
@@ -17,11 +37,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // Preiau datele din formular
-    $email = $_POST["email"];
-    $parola = $_POST["parola"];
+    $email = htmlspecialchars($_POST['email']);
+    $parola = htmlspecialchars($_POST['parola']);
 
     // Verific dacă utilizatorul este activ
-    $verificare_status = "SELECT status, parola FROM utilizatori WHERE email = '$email'";
+    $verificare_status = "SELECT status, parola, nume_utilizator, email, tip_user FROM utilizatori WHERE email = '$email'";
     $rezultat_status = $conn->query($verificare_status);
 
     if ($rezultat_status->num_rows > 0) {
@@ -35,7 +55,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Verific parola
         if (password_verify($parola, $parola_hash)) {
-            header("Location: welcome_spital.php");
+
+            //Preiau numele utilizatorului din db
+            // $query_nume_utilizator = "SELECT nume_utilizator FROM utilizatori WHERE email = '$email'";
+            // $rezultat_nume_utilizator = $conn->query($query_nume_utilizator);
+
+            // if ($rezultat_nume_utilizator->num_rows > 0) {
+            //    $nume_utilizator = $rezultat_nume_utilizator->fetch_assoc()["nume_utilizator"];
+
+                $_SESSION['user'] = array(
+                 'nume_utilizator' => $utilizator["nume_utilizator"],
+                  'email' => $utilizator["email"],
+                 'tip_user' => $utilizator["tip_user"],
+                );
+
+            header("Location: index.php");
             exit();
         } else {
             die("Parola incorectă.");
@@ -46,5 +80,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Închid conexiunea la baza de date
     $conn->close();
+}
 }
 ?>
