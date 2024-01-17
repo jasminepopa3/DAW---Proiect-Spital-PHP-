7259ac5fed55328ee3dd952bfbbfb0cda76ad0e1
jasminepopa3 <47035977+jasminepopa3@users.noreply.github.com>
2024-01-17 23:00:54 +0200

    Update fisa_pacient_print_view.php

--- fisa_pacient_print_view.php
+++ fisa_pacient_print_view.php
@@ -1,38 +1,67 @@
+
+
 <table>
 <tr>
-  <td style="text-align: center"><h1>Fisa pacient</h1></td>
+    <td style='width:50%'><img src="logoo.jpg" alt="logo_spital" width="75" height="60"></td>
+    <td style='width:50%'><p style="text-align: right"><br>Strada Academiei 14,<br> București 010014 </p></td>
 </tr>
 <tr>
-  <td></td>
+  <td colspan="2"></td>
 </tr>
 <tr>
-  <td></td>
+  <td colspan="2" style="text-align: center; background-color: #f2f2f2;"><h1>Fisa pacient</h1></td>
 </tr>
 <tr>
-  <td>Nume pacient: <?php echo $nume_pacient ?></td>
+  <td colspan="2"></td>
 </tr>
 <tr>
-  <td>Prenume pacient: <?php echo $prenume_pacient ?></td>
+  <td colspan="2"></td>
 </tr>
 <tr>
-  <td>Data nasterii: <?php echo $data_nasterii ?></td>
+  <td colspan="2">Nume pacient: <?php echo $nume_pacient ?></td>
 </tr>
 <tr>
-  <td>Sex: <?php echo $sex ?></td>
+  <td colspan="2">Prenume pacient: <?php echo $prenume_pacient ?></td>
 </tr>
 <tr>
-  <td>Inaltime: <?php echo $inaltime ?></td>
+  <td colspan="2">Data nasterii: <?php echo $data_nasterii ?></td>
 </tr>
 <tr>
-  <td>Greutate: <?php echo $greutate ?></td>
+  <td colspan="2">Sex: <?php echo $sex ?></td>
 </tr>
 <tr>
-  <td>Adresa: <?php echo $adresa ?></td>
+  <td colspan="2">Inaltime: <?php echo $inaltime ?></td>
 </tr>
 <tr>
-  <td>CNP: <?php echo $cnp ?></td>
+  <td colspan="2">Greutate: <?php echo $greutate ?></td>
 </tr>
 <tr>
-  <td>Nr. telefon: <?php echo $numar_telefon ?></td>
+  <td colspan="2">Adresa: <?php echo $adresa ?></td>
+</tr>
+<tr>
+  <td colspan="2">CNP: <?php echo $cnp ?></td>
+</tr>
+<tr>
+  <td colspan="2">Nr. telefon: <?php echo $numar_telefon ?></td>
+</tr>
+<tr>
+  <td colspan="2"></td>
+</tr>
+<tr>
+  <td colspan="2"></td>
+</tr>
+<tr>
+    <td width="70%" style="text-align:left">Data,</td>
+    <td width="30%" style="text-align:right">Parafa unității medicale,</td>
+</tr>
+
+<?php
+date_default_timezone_set('Europe/Bucharest');
+$dataCurenta = date('Y-m-d');
+?>
+
+<tr>
+    <td width="80%"><?php echo $dataCurenta ?></td>
+    <td width="20%"><img src="parafa.png" alt="parafa_spital" width="200" height="190"></td>
 </tr>
 </table>
